5226e80de64d3c1c35ca8e3d551691cd4c8efa88
Martin Ortega <martin.ortega@mail.rakuten.com>
2013-06-12 22:42:51 -0700

    Added colorful unicode characters

--- index.js
+++ index.js
@@ -4,38 +4,53 @@
 
 var colors = require('colors');
 
-module.exports = function Spinner(duringMessage, afterMessage) {
-  afterMessage = afterMessage || duringMessage;
+module.exports = function Spinner(duringMessage, successMessage, errorMessage) {
+  successMessage = successMessage || duringMessage;
+  errorMessage   = errorMessage   || successMessage;
 
-  var notStarted   = true
-    , states       = ['-', '\\', '|', '/']
-    , currentState
+  var bars       = ['-', '\\', '|', '/']
+    , currentBar = 0
+    , state      = 'waiting'
     , timer;
 
   function start() {
-    if (notStarted) {
-      notStarted = false;
-      currentState = 0;
+    if (state === 'waiting') {
+      state = 'running';
       tick();
     }
   }
 
   function end() {
-    clearTimeout(timer);
-    process.stdout.clearLine();
-    process.stdout.write('\r+ ' + afterMessage + '\n');
+    if (state === 'running') {
+      state = 'done';
+      clearTimeout(timer);
+      process.stdout.clearLine();
+      process.stdout.write('\r\u2713 '.bold.green + successMessage + '\n');
+    }
+  }
+
+  function error() {
+    if (state === 'running') {
+      state = 'done';
+      clearTimeout(timer);
+      process.stdout.clearLine();
+      process.stdout.write('\r\u2718 '.bold.red + errorMessage + '\n');
+    }
   }
 
   function tick() {
-    var bar = states[currentState++ % states.length];
-    process.stdout.write('\r' + bar.bold.cyan + ' ' + duringMessage + ' ');
-    timer = setTimeout(tick, 200);
+    if (state === 'running') {
+      var bar = bars[currentBar++ % bars.length];
+      process.stdout.write('\r' + bar.bold.cyan + ' ' + duringMessage + ' ');
+      timer = setTimeout(tick, 200);
+    }
   }
 
   //------------------------------------------------------------------------
 
   return {
     start : start,
-    end   : end
+    end   : end,
+    error : error
   };
 };
